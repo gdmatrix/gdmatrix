12cfd33f6aaf6a5e4168ca417f6d06fc51e013b0
realor <realor@santfeliu.cat>
2020-03-31 08:59:03 +0200

    Start gdmatrix-client.bat on install without elevated privileges.

--- gdmatrix-client/src/main/deploy/einstall.bat
+++ gdmatrix-client/src/main/deploy/einstall.bat
@@ -1,5 +1,16 @@
 @echo off
+set app_name=%1
 set inst_dir=%~dp0
 cd %inst_dir%
-Elevate64.exe install.bat
 
+reg add HKCR\matrix /f /t REG_SZ /d "URL:Matrix Protocol" 2> nul
+reg add HKCR\matrix /f /v "URL Protocol" /t REG_SZ /d ""
+reg add HKCR\matrix\shell\open\command /f /t REG_SZ /d "C:\ProgramData\gdmatrix\client\%app_name%.bat %%1"
+
+rmdir /s /q "%programdata%"\gdmatrix\jre
+rmdir /s /q "%programdata%"\gdmatrix\client
+xcopy /s /i "%inst_dir%"\jre "%programdata%"\gdmatrix\jre
+xcopy /s /i "%inst_dir%"\lib "%programdata%"\gdmatrix\client\lib
+
+echo cd "%programdata%"\gdmatrix\client > "%programdata%"\gdmatrix\client\%app_name%.bat
+echo start ..\jre\bin\javaw -cp lib/* org.santfeliu.matrix.client.MatrixClient %%1 >> "%programdata%"\gdmatrix\client\%app_name%.bat
--- gdmatrix-client/src/main/deploy/install.bat
+++ gdmatrix-client/src/main/deploy/install.bat
@@ -4,36 +4,9 @@ set app_name=gdmatrix-client
 set inst_dir=%~dp0
 cd %inst_dir%
 
-reg add HKCR\matrix /f /t REG_SZ /d "URL:Matrix Protocol" 2> nul
-if %errorlevel% neq 0 goto :error
-reg add HKCR\matrix /f /v "URL Protocol" /t REG_SZ /d ""
-reg add HKCR\matrix\shell\open\command /f /t REG_SZ /d "C:\ProgramData\gdmatrix\client\%app_name%.bat %%1"
-
-rmdir /s /q "%programdata%"\gdmatrix\jre
-rmdir /s /q "%programdata%"\gdmatrix\client
-xcopy /s /i "%inst_dir%"\jre "%programdata%"\gdmatrix\jre
-xcopy /s /i "%inst_dir%"\lib "%programdata%"\gdmatrix\client\lib
-
-echo cd "%programdata%"\gdmatrix\client > "%programdata%"\gdmatrix\client\%app_name%.bat
-echo start ..\jre\bin\javaw -cp lib/* org.santfeliu.matrix.client.MatrixClient %%1 >> "%programdata%"\gdmatrix\client\%app_name%.bat
+elevate.exe -w einstall.bat %app_name%
 
 if "%1" == "silent" goto :end
 call "%programdata%"\gdmatrix\client\%app_name%.bat
-goto :end
-
-:error
-echo ==================================================================
-echo   You must run this installer as administrator.
-echo   Right click on this file and select "Run as administrator".
-echo ------------------------------------------------------------------
-echo   Debes ejecutar este instalador como administrador.       
-echo   Pulsa el boton derecho del raton sobre este fichero y selecciona 
-echo   "Ejecutar como administrador"
-echo ------------------------------------------------------------------
-echo   Has d'executar aquest instal.lador com a administrador.
-echo   Prem el buto dret del ratoli sobre aquest fitxer i selecciona
-echo   "Executa com a administrador"
-echo ==================================================================
-pause
 
 :end
