39fbd2908c84ae87c53a15527c368ea731bf4107
realor <realor@santfeliu.cat>
2020-04-02 06:09:56 +0200

    Fix GDMatrixClient installer for Windows so that it works when its name contains special symbols.

--- gdmatrix-client/src/main/deploy/install.bat
+++ gdmatrix-client/src/main/deploy/install.bat
@@ -4,7 +4,7 @@ set app_name=gdmatrix-client
 set inst_dir=%~dp0
 cd %inst_dir%
 
-elevate.exe -w einstall.bat %app_name%
+elevate.exe -c -w einstall.bat %app_name%
 
 if "%1" == "silent" goto :end
 call "%programdata%"\gdmatrix\client\%app_name%.bat
