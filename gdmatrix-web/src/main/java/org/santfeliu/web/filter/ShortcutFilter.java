b5ed86a6e264f757415836513542c47451daf6a2
realor <realor@santfeliu.cat>
2021-09-16 12:55:06 +0200

    Modify ShortcutFilter to allow redirects based on the host header. -The shortcuts file accepts entries with format: <host>/<uri>

--- gdmatrix-web/src/main/java/org/santfeliu/web/filter/ShortcutFilter.java
+++ gdmatrix-web/src/main/java/org/santfeliu/web/filter/ShortcutFilter.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.web.filter;
@@ -36,6 +36,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -59,7 +60,7 @@ import org.santfeliu.web.HttpUtils;
 public class ShortcutFilter implements Filter
 {
   public static final String WILDCARD = "*";
-  static final Logger logger = Logger.getLogger("ShortcutFilter");
+  static final Logger LOGGER = Logger.getLogger("ShortcutFilter");
   public static final String URL_PARAM =
     ShortcutFilter.class.getName() + ".url";
   public static final String UPDATE_INTERVAL_PARAM =
@@ -69,6 +70,35 @@ public class ShortcutFilter implements Filter
   private int updateInterval = 300000; // 5 minutes
   private long lastUpdate;
   private URL shortcutsUrl;
+  private static final HashSet<String> reservedUris = new HashSet<>();
+
+  static
+  {
+    reservedUris.add("/documents");
+    reservedUris.add("/services");
+    reservedUris.add("/wsdirectory");
+    reservedUris.add("/scanner");
+    reservedUris.add("/classif");
+    reservedUris.add("/rss");
+    reservedUris.add("/feed");
+    reservedUris.add("/events");
+    reservedUris.add("/signatures");
+    reservedUris.add("/qrcode");
+    reservedUris.add("/proxy");
+    reservedUris.add("/form");
+    reservedUris.add("/scripts");
+    reservedUris.add("/rest");
+    reservedUris.add("/valid");
+    reservedUris.add("/pagament");
+    reservedUris.add("/clock");
+    reservedUris.add("/fckfaces");
+    reservedUris.add("/reports");
+    reservedUris.add("/imgscale");
+    reservedUris.add("/work");
+    reservedUris.add("/templates");
+    reservedUris.add("/themes");
+    reservedUris.add("/common");
+  }
 
   @Override
   public void init(FilterConfig filterConfig) throws ServletException
@@ -88,7 +118,7 @@ public class ShortcutFilter implements Filter
       }
       catch (MalformedURLException ex)
       {
-        logger.log(Level.SEVERE, "Init failed", ex);
+        LOGGER.log(Level.SEVERE, "Init failed", ex);
       }
     }
   }
@@ -155,7 +185,7 @@ public class ShortcutFilter implements Filter
           Properties newShortcuts = new Properties();
           newShortcuts.load(is);
           shortcuts = newShortcuts;
-          logger.log(Level.INFO, "Update completed: {0} shortcuts",
+          LOGGER.log(Level.INFO, "Update completed: {0} shortcuts",
             new Object[]{ shortcuts.size() });
         }
         finally
@@ -163,29 +193,49 @@ public class ShortcutFilter implements Filter
           is.close();
         }
       }
-      catch (Exception ex)
+      catch (IOException ex)
       {
-        logger.log(Level.SEVERE, "Update failed", ex);
+        LOGGER.log(Level.SEVERE, "Update failed", ex);
       }
     }
   }
-  
+
   private String getLocation(HttpServletRequest request)
   {
+    String host = request.getHeader("Host");
     String uri = request.getServletPath();
-    if (uri.endsWith(".faces")) return null;
+
+    if (uri.endsWith(".faces") || reservedUris.contains(uri))
+      return null;
+
     if (uri.endsWith("/")) uri = uri.substring(0, uri.length() - 1);
-    String location = shortcuts.getProperty(uri);
+
+    String location = shortcuts.getProperty(host + uri);
+    if (location == null)
+    {
+      location = shortcuts.getProperty(uri);
+    }
+
     int index = uri.lastIndexOf("/");
     while (location == null && index > 0)
     {
       String wildcardUri = uri.substring(0, index + 1) + WILDCARD;
-      location = shortcuts.getProperty(wildcardUri);
+      location = shortcuts.getProperty(host + wildcardUri);
+      if (location == null)
+      {
+        location = shortcuts.getProperty(wildcardUri);
+      }
       index = uri.lastIndexOf("/", index - 1);
     }
-    return location == null ? null : mergeLocation(request, location);
+    if (location != null)
+    {
+      location = mergeLocation(request, location);
+      LOGGER.log(Level.INFO, "Redirect [{0}] to [{1}]",
+        new String[]{host + uri, location});
+    }
+    return location;
   }
-  
+
   private String mergeLocation(HttpServletRequest request, String location)
   {
     if (location.contains("${"))
@@ -197,7 +247,7 @@ public class ShortcutFilter implements Filter
       variables.put("queryString", request.getQueryString());
       variables.put("path", uri.substring(1).split("/"));
       JSTemplate template = new JSTemplate(location, WebScriptableBase.class);
-      return template.merge(variables);   
+      return template.merge(variables);
     }
     return location;
   }
