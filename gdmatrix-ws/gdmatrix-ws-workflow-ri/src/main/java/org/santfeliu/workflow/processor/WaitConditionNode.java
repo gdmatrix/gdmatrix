290195c5c21b0f222a59844477d08208775f738d
realor <realor@santfeliu.cat>
2022-01-27 08:22:17 +0100

    Put node information in thread name for workflow nodes based on js.

--- gdmatrix-ws/gdmatrix-ws-workflow-ri/src/main/java/org/santfeliu/workflow/processor/WaitConditionNode.java
+++ gdmatrix-ws/gdmatrix-ws-workflow-ri/src/main/java/org/santfeliu/workflow/processor/WaitConditionNode.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.workflow.processor;
@@ -42,17 +42,23 @@ import org.santfeliu.workflow.WorkflowInstance;
  *
  * @author realor
  */
-public class WaitConditionNode 
-  extends org.santfeliu.workflow.node.WaitConditionNode 
+public class WaitConditionNode
+  extends org.santfeliu.workflow.node.WaitConditionNode
   implements NodeProcessor
 {
   @Override
   public String process(WorkflowInstance instance, WorkflowActor actor)
     throws Exception
   {
+    Thread thread = Thread.currentThread();
+    String threadName = thread.getName();
+
     Context cx = ContextFactory.getGlobal().enterContext();
     try
     {
+      thread.setName("workflow:" + instance.getWorkflowName()
+        + ":" + getId() + ":" + instance.getInstanceId());
+
       String mcondition = Template.create(condition).merge(instance);
       Scriptable scope = new ScriptableBase(cx, instance);
       Object result = cx.evaluateString(scope, mcondition, "<cond>", 1, null);
@@ -65,6 +71,7 @@ public class WaitConditionNode
     }
     finally
     {
+      thread.setName(threadName);
       Context.exit();
     }
   }
