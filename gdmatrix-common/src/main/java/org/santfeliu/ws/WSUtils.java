f1a2f4b741c55ef6fbb5c285f286e94f460c9ae1
realor <realor@santfeliu.cat>
2022-03-16 19:09:23 +0100

    Change jaxws libraries and wsimport maven plugin to build with jdk11.

--- gdmatrix-common/src/main/java/org/santfeliu/ws/WSUtils.java
+++ gdmatrix-common/src/main/java/org/santfeliu/ws/WSUtils.java
@@ -1,36 +1,37 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.ws;
 
 import com.sun.xml.ws.api.server.WSEndpoint;
+import static com.sun.xml.ws.developer.JAXWSProperties.HTTP_REQUEST_URL;
 import com.sun.xml.ws.transport.http.servlet.ServletAdapter;
 import com.sun.xml.ws.transport.http.servlet.WSServlet;
 import com.sun.xml.ws.transport.http.servlet.WSServletDelegate;
@@ -88,8 +89,11 @@ public class WSUtils
   public static ServletAdapter getServletAdapter(
     WebServiceContext wsContext)
   {
+    MessageContext messageContext = wsContext.getMessageContext();
+    System.out.println(messageContext);
+
     String pathInfo =
-      (String)wsContext.getMessageContext().get(MessageContext.PATH_INFO);
+      (String)wsContext.getMessageContext().get(HTTP_REQUEST_URL);
 
     ServletAdapter servletAdapter = null;
     List<ServletAdapter> adapters = getServletAdapters(wsContext);
@@ -98,7 +102,7 @@ public class WSUtils
     while (iter.hasNext() && servletAdapter == null)
     {
       ServletAdapter sa = iter.next();
-      if (sa.getValidPath().endsWith(pathInfo))
+      if (pathInfo.endsWith(sa.getValidPath()))
       {
         servletAdapter = sa;
       }
