e65fd40a721b698e7b206e8e4dab3be8f6c33451
lopezrj-sf <lopezrj@ext.santfeliu.cat>
2022-09-27 13:52:44 +0200

    Add new kernel pages.
    - Added 'person_addresses' page.
    - Added 'person_contacts' page.
    - Added 'person_cases' page.

---
+++ gdmatrix-web/src/main/java/org/matrix/pf/kernel/PersonAddressesBacking.java
@@ -0,0 +1,404 @@
+/*
+ * GDMatrix
+ *  
+ * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
+ *  
+ * This program is licensed and may be used, modified and redistributed under 
+ * the terms of the European Public License (EUPL), either version 1.1 or (at 
+ * your option) any later version as soon as they are approved by the European 
+ * Commission.
+ *  
+ * Alternatively, you may redistribute and/or modify this program under the 
+ * terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation; either  version 3 of the License, or (at your option) 
+ * any later version. 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *    
+ * See the licenses for the specific language governing permissions, limitations 
+ * and more details.
+ *    
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
+ * with this program; if not, you may find them at: 
+ *    
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * http://www.gnu.org/licenses/ 
+ * and 
+ * https://www.gnu.org/licenses/lgpl.txt
+ */
+package org.matrix.pf.kernel;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.model.SelectItem;
+import javax.inject.Named;
+import org.matrix.kernel.AddressFilter;
+import org.matrix.kernel.AddressView;
+import org.matrix.kernel.ContactView;
+import org.matrix.kernel.KernelManagerPort;
+import org.matrix.kernel.PersonAddress;
+import org.matrix.kernel.PersonAddressFilter;
+import org.matrix.kernel.PersonAddressView;
+import org.matrix.pf.web.PageBacking;
+import org.matrix.pf.web.helper.ResultListHelper;
+import org.matrix.pf.web.helper.ResultListPage;
+import org.matrix.pf.web.helper.TabHelper;
+import org.matrix.pf.web.helper.TabPage;
+import org.matrix.web.WebUtils;
+import org.primefaces.event.SelectEvent;
+import org.santfeliu.kernel.web.KernelConfigBean;
+
+/**
+ *
+ * @author lopezrj-sf
+ */
+@Named
+public class PersonAddressesBacking extends PageBacking 
+  implements TabPage, ResultListPage
+{  
+  private static final String PERSON_BACKING = "personBacking";
+  private static final String ADDRESS_BACKING = "addressBacking";
+  private static final String OUTCOME = "pf_person_addresses";   
+  
+  private PersonBacking personBacking;
+  
+  //Helpers
+  private ResultListHelper<PersonAddressView> resultListHelper;
+  private TabHelper tabHelper;
+  
+  private PersonAddress editing;
+  
+  private SelectItem addressSelectItem;
+  
+  public PersonAddressesBacking()
+  {
+    //Let to super class constructor.   
+  }
+  
+  @PostConstruct
+  public void init()
+  {
+    personBacking = WebUtils.getBacking(PERSON_BACKING);
+    resultListHelper = new ResultListHelper(this);
+    tabHelper = new TabHelper(this);
+    populate();
+  }
+
+  public PersonAddress getEditing()
+  {
+    return editing;
+  }
+
+  public void setEditing(PersonAddress editing)
+  {
+    this.editing = editing;
+  } 
+
+  public boolean isNew()
+  {
+    return isNew(editing);
+  }
+  
+  @Override
+  public String getPageObjectId()
+  {
+    if (editing != null)
+      return editing.getPersonAddressId();
+    else
+      return null;
+  }
+  
+  public String getPageObjectDescription()
+  {
+    if (editing != null)
+    {
+      AddressBacking addressBacking = WebUtils.getBacking(ADDRESS_BACKING);
+      return addressBacking.getDescription(editing.getAddressId());
+    }
+    return null;
+  }
+
+  @Override
+  public PersonBacking getObjectBacking()
+  {
+    return personBacking;
+  }
+
+  @Override
+  public ResultListHelper<PersonAddressView> getResultListHelper()
+  {
+    return resultListHelper;
+  }  
+  
+  public TabHelper getTabHelper()
+  {
+    return tabHelper;
+  }
+  
+  public List<PersonAddressView> getRows()
+  {
+    return resultListHelper.getRows();
+  }
+  
+  //Address selection
+  public SelectItem getAddressSelectItem()
+  {
+    return addressSelectItem;
+  }
+  
+  public void setAddressSelectItem(SelectItem item)
+  {
+    addressSelectItem = item;
+  }  
+
+  public void onAddressSelect(SelectEvent<SelectItem> event) 
+  {
+    SelectItem item = event.getObject();
+    String addressId = (String) item.getValue();
+    editing.setAddressId(addressId);
+  }  
+  
+  public List<SelectItem> completeAddress(String query)
+  {
+    return completeAddress(query, editing.getAddressId());
+  }  
+  
+  public List<SelectItem> getFavorites()
+  {
+    AddressBacking addressBacking = WebUtils.getBacking(ADDRESS_BACKING);
+    return addressBacking.getFavorites();     
+  }  
+  
+  private List<SelectItem> completeAddress(String query, String addressId)
+  {
+    ArrayList<SelectItem> items = new ArrayList();
+    AddressBacking addressBacking = WebUtils.getBacking(ADDRESS_BACKING);
+    
+    //Add current item
+    if (!isNew(editing))
+    {
+      String description = "";
+      if (addressId != null)
+        description = addressBacking.getDescription(addressId);
+      items.add(new SelectItem(addressId, description));
+    }
+        
+    //Query search
+    if (query != null && query.length() >= 3)
+    {
+      AddressFilter filter = new AddressFilter();
+      filter.setStreetName("\"" + query + "\"");
+      filter.setMaxResults(10);
+      List<AddressView> addresss = 
+        KernelConfigBean.getPort().findAddressViews(filter);
+      if (addresss != null)
+      {       
+        for (AddressView address : addresss)
+        {
+          String description = addressBacking.getDescription(address);
+          SelectItem item = new SelectItem(address.getAddressId(), description);
+          items.add(item);
+        }
+      }
+    }
+    else
+    {
+      //Add favorites
+      items.addAll(addressBacking.getFavorites()); 
+    }
+    
+    return items;
+  }  
+    
+  public String getContactIcon(ContactView contact)
+  {
+    return KernelUtils.getContactTypeIconName(contact.getContactTypeId());
+  }
+
+  public void setSelectedAddress(String addressId)
+  {
+    editing.setAddressId(addressId);
+    showDialog();
+  }
+  
+  @Override
+  public String show(String pageObjectId)
+  {
+    editAddress(pageObjectId);
+    showDialog();
+    return isEditing(pageObjectId) ? OUTCOME : show();
+  }  
+  
+  @Override
+  public String show()
+  {    
+    populate();
+    return OUTCOME;
+  }
+  
+  public String editAddress(PersonAddressView row)
+  {
+    String personAddressId = null;
+    if (row != null)
+      personAddressId = row.getPersonAddressId();
+
+    return editAddress(personAddressId);
+  } 
+  
+  public String createAddress()
+  {
+    editing = new PersonAddress();
+    return null;
+  }  
+  
+  public String removeAddress(PersonAddressView row)
+  {
+    try
+    {
+      if (row == null)
+        throw new Exception("ADDRESS_MUST_BE_SELECTED");
+      
+      String rowPersonAddressId = row.getPersonAddressId();
+      
+      if (editing != null && 
+        rowPersonAddressId.equals(editing.getPersonAddressId()))
+        editing = null;
+      
+      KernelManagerPort port = KernelConfigBean.getPort();
+      port.removePersonAddress(rowPersonAddressId);
+      
+      return show();
+    }
+    catch (Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }  
+
+  public String storeAddress()
+  {
+    try
+    {
+      if (editing == null)
+        return null;
+      
+      //Address must be selected
+      if (editing.getAddressId() == null || editing.getAddressId().isEmpty())
+      {
+        throw new Exception("ADDRESS_MUST_BE_SELECTED"); 
+      }
+                            
+      String personId = personBacking.getObjectId();
+      editing.setPersonId(personId);
+      KernelManagerPort port = KernelConfigBean.getPort();
+      port.storePersonAddress(editing);
+      cancel();
+      return show();
+    }
+    catch (Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }
+    
+
+  @Override
+  public List<PersonAddressView> getResults(int firstResult, int maxResults)
+  {
+    try
+    {
+      PersonAddressFilter filter = new PersonAddressFilter();
+      filter.setPersonId(personBacking.getObjectId());        
+      filter.setFirstResult(firstResult);
+      filter.setMaxResults(maxResults);
+      return KernelConfigBean.getPort().findPersonAddressViews(filter);
+    }
+    catch (Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }
+
+  @Override
+  public String store()
+  {
+    return storeAddress();
+  }
+  
+  @Override
+  public void load()
+  {
+    resultListHelper.search();
+  }
+
+  @Override
+  public void create()
+  {
+    editing = new PersonAddress();
+  }
+  
+  @Override
+  public String cancel()
+  {
+    editing = null;
+    addressSelectItem = null;
+    return null;
+  }  
+  
+  @Override
+  public void reset()
+  {
+    cancel();
+    resultListHelper.reset();
+  }
+    
+  private boolean isNew(PersonAddress personAddress)
+  {
+    return (personAddress != null && 
+      personAddress.getPersonAddressId() == null);
+  }  
+    
+  private String editAddress(String personAddressId)
+  {
+    try
+    {
+      if (personAddressId != null && !isEditing(personAddressId))
+      {
+        editing = KernelConfigBean.getPort().loadPersonAddress(personAddressId);
+        loadAddressSelectItem();     
+      }
+      else if (personAddressId == null)
+      {
+        editing = new PersonAddress();
+      }
+    }
+    catch(Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }  
+
+  private void loadAddressSelectItem()
+  {
+    if (editing != null)
+    {
+      AddressBacking addressBacking = WebUtils.getBacking(ADDRESS_BACKING);
+      
+      if (editing.getAddressId() != null)
+      {
+        String description = 
+          addressBacking.getDescription(editing.getAddressId());
+        addressSelectItem = 
+          new SelectItem(editing.getAddressId(), description);
+      }
+    }
+  }  
+
+}
---
+++ gdmatrix-web/src/main/java/org/matrix/pf/kernel/PersonContactsBacking.java
@@ -0,0 +1,328 @@
+/*
+ * GDMatrix
+ *  
+ * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
+ *  
+ * This program is licensed and may be used, modified and redistributed under 
+ * the terms of the European Public License (EUPL), either version 1.1 or (at 
+ * your option) any later version as soon as they are approved by the European 
+ * Commission.
+ *  
+ * Alternatively, you may redistribute and/or modify this program under the 
+ * terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation; either  version 3 of the License, or (at your option) 
+ * any later version. 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *    
+ * See the licenses for the specific language governing permissions, limitations 
+ * and more details.
+ *    
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
+ * with this program; if not, you may find them at: 
+ *    
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * http://www.gnu.org/licenses/ 
+ * and 
+ * https://www.gnu.org/licenses/lgpl.txt
+ */
+package org.matrix.pf.kernel;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import org.matrix.kernel.Contact;
+import org.matrix.kernel.ContactFilter;
+import org.matrix.kernel.ContactView;
+import org.matrix.kernel.KernelManagerPort;
+import org.matrix.pf.web.PageBacking;
+import org.matrix.pf.web.helper.ResultListHelper;
+import org.matrix.pf.web.helper.ResultListPage;
+import org.matrix.pf.web.helper.TabHelper;
+import org.matrix.pf.web.helper.TypedHelper;
+import org.matrix.pf.web.helper.TypedTabPage;
+import org.matrix.web.WebUtils;
+import org.santfeliu.kernel.web.KernelConfigBean;
+
+/**
+ *
+ * @author lopezrj-sf
+ */
+@Named
+public class PersonContactsBacking extends PageBacking 
+  implements TypedTabPage, ResultListPage
+{
+  private static final String PERSON_BACKING = "personBacking";
+  private static final String ROOT_TYPE_ID = "Contact";
+  private static final String OUTCOME = "pf_person_contacts";  
+  
+  private PersonBacking personBacking;
+  
+  //Helpers
+  private TypedHelper typedHelper;
+  private ResultListHelper<ContactView> resultListHelper;
+  private TabHelper tabHelper;
+  
+  private ContactView editing;
+  
+  public PersonContactsBacking()
+  {
+    //Let to super class constructor.   
+  }
+  
+  @PostConstruct
+  public void init()
+  {
+    personBacking = WebUtils.getBacking(PERSON_BACKING);   
+    typedHelper = new TypedHelper(this);
+    resultListHelper = new ResultListHelper(this);
+    tabHelper = new TabHelper(this);
+    populate();
+  }
+  
+  public ContactView getEditing()
+  {
+    return editing;
+  }
+
+  public void setEditing(ContactView editing)
+  {
+    this.editing = editing;
+  } 
+  
+  public boolean isNew()
+  {
+    return isNew(editing);
+  }
+  
+  @Override
+  public String getPageObjectId()
+  {
+    if (editing != null)
+      return editing.getContactId();
+    else
+      return null;
+  }
+
+  @Override
+  public String getRootTypeId()
+  {
+    return ROOT_TYPE_ID;
+  }
+  
+  public String getPageObjectDescription()
+  {
+    if (editing != null)
+    {
+      //AddressBacking addressBacking = WebUtils.getBacking(ADDRESS_BACKING);
+      //return addressBacking.getDescription(editing.getAddressId());
+      return editing.getContactId();
+    }
+    return null;
+  }
+
+  @Override
+  public PersonBacking getObjectBacking()
+  {
+    return personBacking;
+  }
+  
+  @Override
+  public String getTypeId()
+  {
+    return personBacking.getPageTypeId();
+  }
+  
+  @Override
+  public ResultListHelper<ContactView> getResultListHelper()
+  {
+    return resultListHelper;
+  }  
+  
+  @Override
+  public TypedHelper getTypedHelper()
+  {
+    return typedHelper;
+  }  
+
+  public TabHelper getTabHelper()
+  {
+    return tabHelper;
+  }
+  
+  public List<ContactView> getRows()
+  {
+    return resultListHelper.getRows();
+  }
+    
+  public String getContactIcon(ContactView contact)
+  {
+    return KernelUtils.getContactTypeIconName(contact.getContactTypeId());
+  }
+  
+  @Override
+  public String show(String pageObjectId)
+  {
+    editContact(pageObjectId);
+    showDialog();
+    return isEditing(pageObjectId) ? OUTCOME : show();
+  }  
+  
+  @Override
+  public String show()
+  {    
+    populate();
+    return OUTCOME;
+  }
+  
+  public String editContact(ContactView row)
+  {
+    String contactId = null;
+    if (row != null)
+      contactId = row.getContactId();
+
+    return editContact(contactId);
+  } 
+  
+  public String createContact()
+  {
+    editing = new ContactView();
+    return null;
+  }  
+  
+  public String removeContact(ContactView row)
+  {
+    try
+    {
+      if (row == null)
+        throw new Exception("CONTACT_MUST_BE_SELECTED");
+      
+      String rowContactId = row.getContactId();
+      
+      if (editing != null && 
+        rowContactId.equals(editing.getContactId()))
+        editing = null;
+      
+      KernelManagerPort port = KernelConfigBean.getPort();
+      port.removeContact(rowContactId);
+      
+      return show();
+    }
+    catch (Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }  
+
+  public String storeContact()
+  {
+    try
+    {
+      if (editing == null)
+        return null;
+      
+      if (editing.getContactTypeId() == null)
+      {
+        editing.setContactTypeId(typedHelper.getTypeId());
+      }      
+      Contact contact = new Contact();
+      contact.setContactId(editing.getContactId());
+      contact.setPersonId(personBacking.getObjectId());
+      contact.setContactTypeId(editing.getContactTypeId());
+      contact.setValue(editing.getValue());
+      contact.setComments(editing.getComments());
+      KernelConfigBean.getPort().storeContact(contact);      
+      cancel();
+      return show();
+    }
+    catch (Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }
+
+  @Override
+  public List<ContactView> getResults(int firstResult, int maxResults)
+  {
+    try
+    {
+      ContactFilter filter = new ContactFilter();
+      filter.setPersonId(personBacking.getObjectId());        
+      filter.setFirstResult(firstResult);
+      filter.setMaxResults(maxResults);
+      return KernelConfigBean.getPort().findContactViews(filter);
+    }
+    catch (Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }
+  
+  @Override
+  public String store()
+  {
+    return storeContact();
+  }
+  
+  @Override
+  public void load()
+  {
+    resultListHelper.search();
+  }
+
+  @Override
+  public void create()
+  {
+    editing = new ContactView();
+  }
+  
+  @Override
+  public String cancel()
+  {
+    editing = null;
+    return null;
+  }  
+  
+  @Override
+  public void reset()
+  {
+    cancel();
+    resultListHelper.reset();
+  }
+    
+  private boolean isNew(ContactView contactView)
+  {
+    return (contactView != null && contactView.getContactId() == null);
+  }  
+    
+  private String editContact(String contactId)
+  {
+    try
+    {
+      if (contactId != null && !isEditing(contactId))
+      {
+        Contact contact = KernelConfigBean.getPort().loadContact(contactId);
+        editing = new ContactView();        
+        editing.setContactId(contactId);
+        editing.setContactTypeId(contact.getContactTypeId());
+        editing.setValue(contact.getValue());
+        editing.setComments(contact.getComments());                
+      }
+      else
+      {
+        editing = new ContactView();
+      }
+    }
+    catch(Exception ex)
+    {
+      error(ex);
+    }
+    return null;
+  }  
+
+}
