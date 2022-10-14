a322a03282f4ecf870fa767b8025b39a44c66c62
realor <realor@santfeliu.cat>
2022-10-14 17:47:03 +0200

    Clean up code.

--- gdmatrix-web/src/main/java/org/santfeliu/faces/convert/EnumConverter.java
+++ gdmatrix-web/src/main/java/org/santfeliu/faces/convert/EnumConverter.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.faces.convert;
@@ -39,7 +39,7 @@ import javax.faces.convert.ConverterException;
 
 /**
  *
- * @author unknown
+ * @author realor
  */
 public class EnumConverter implements Converter, Serializable
 {
@@ -47,7 +47,8 @@ public class EnumConverter implements Converter, Serializable
   {
   }
 
-  public Object getAsObject(FacesContext context, 
+  @Override
+  public Object getAsObject(FacesContext context,
     UIComponent component, String value) throws ConverterException
   {
     if (value == null || value.trim().length() == 0)
@@ -71,7 +72,6 @@ public class EnumConverter implements Converter, Serializable
     int i = 0;
     while (!found && i < constants.length)
     {
-      System.out.println(constants[i].toString());
       if (constants[i].toString().equals(value)) found = true;
       else i++;
     }
@@ -79,7 +79,8 @@ public class EnumConverter implements Converter, Serializable
     return constants[i];
   }
 
-  public String getAsString(FacesContext context, 
+  @Override
+  public String getAsString(FacesContext context,
     UIComponent component, Object value) throws ConverterException
   {
     if (value == null) return "";
