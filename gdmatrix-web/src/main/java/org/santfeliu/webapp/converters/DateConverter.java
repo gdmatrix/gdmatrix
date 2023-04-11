24f298ff0b1017435fef3e705dc9e5b0af7e2721
realor <realor@santfeliu.cat>
2023-04-11 12:48:32 +0200

    Add date filters in security pages. -Make DatePickerConverter more flexible for dateTime format.

--- gdmatrix-web/src/main/java/org/santfeliu/webapp/converters/DateConverter.java
+++ gdmatrix-web/src/main/java/org/santfeliu/webapp/converters/DateConverter.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.webapp.converters;
@@ -49,89 +49,83 @@ public class DateConverter implements Converter<String>, Serializable
   protected static final String INTERNAL_DATETIME_FORMAT = "yyyyMMddHHmmss";
   protected static final String INTERNAL_DATE_FORMAT = "yyyyMMdd";
   protected static final String INTERNAL_TIME_FORMAT = "HHmmss";
-  
+
   protected static final String USER_DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
   protected static final String USER_DATE_FORMAT = "dd/MM/yyyy";
   protected static final String USER_TIME_FORMAT = "HH:mm:ss";
-    
+
   @Override
-  public String getAsString(FacesContext fc, UIComponent uic, 
-    String internalDate)    
+  public String getAsString(FacesContext fc, UIComponent uic,
+    String internalDate)
   {
     String userFormat = getUserFormatFromInternalDate(internalDate);
-    
+
     return getAsString(fc, internalDate, userFormat);
   }
 
   @Override
   public String getAsObject(FacesContext fc, UIComponent uic, String userDate)
-  { 
+  {
     String userFormat = getUserFormatFromUserDate(userDate);
-    String internalFormat = getInternalFormatFromUserDate(userDate);  
-    
+    String internalFormat = getInternalFormatFromUserDate(userDate);
+
     return getAsObject(fc, userDate, userFormat, internalFormat);
   }
-  
-  protected String getAsString(FacesContext fc, String internalDate, 
+
+  protected String getAsString(FacesContext fc, String internalDate,
     String userFormat)
   {
-    if (internalDate != null)
-    {      
-      Date date = TextUtils.parseInternalDate(internalDate);
-      Locale locale = fc.getViewRoot().getLocale();
-      return TextUtils.formatDate(date, userFormat, locale);
-    }
-    else
-      return internalDate;      
+    if (internalDate == null) return null;
+
+    Date date = TextUtils.parseInternalDate(internalDate);
+    Locale locale = fc.getViewRoot().getLocale();
+    return TextUtils.formatDate(date, userFormat, locale);
   }
-  
-  protected String getAsObject(FacesContext fc, String userDate, 
+
+  protected String getAsObject(FacesContext fc, String userDate,
     String userFormat, String internalFormat)
   {
     Date date = TextUtils.parseUserDate(userDate, userFormat);
-    if (date != null)
-    {
-      Locale locale = fc.getViewRoot().getLocale();
-      return TextUtils.formatDate(date, internalFormat, locale);
-    }
-    else
-      return userDate;      
+    if (date == null) return null;
+
+    Locale locale = fc.getViewRoot().getLocale();
+    return TextUtils.formatDate(date, internalFormat, locale);
   }
-  
+
   private String getUserFormatFromInternalDate(String internalDate)
   {
     if (internalDate == null)
-      return USER_DATE_FORMAT;    
+      return USER_DATE_FORMAT;
     if (internalDate.length() == 8)
       return USER_DATE_FORMAT;
     if (internalDate.length() == 14)
       return USER_DATETIME_FORMAT;
-    
+
     return USER_DATE_FORMAT;
   }
-    
+
   private String getInternalFormatFromUserDate(String userDate)
   {
     if (userDate == null)
-      return INTERNAL_DATE_FORMAT;    
+      return INTERNAL_DATE_FORMAT;
     if (userDate.length() == 10)
       return INTERNAL_DATE_FORMAT;
     if (userDate.length() == 19)
       return INTERNAL_DATETIME_FORMAT;
-    
-    return INTERNAL_DATE_FORMAT;    
+
+    return INTERNAL_DATE_FORMAT;
   }
-  
+
   private String getUserFormatFromUserDate(String userDate)
-  { 
+  {
     if (userDate == null)
-      return USER_DATE_FORMAT;    
+      return USER_DATE_FORMAT;
     if (userDate.length() == 10)
       return USER_DATE_FORMAT;
     if (userDate.length() == 19)
       return USER_DATETIME_FORMAT;
-    
-    return USER_DATE_FORMAT;     
+
+    return USER_DATE_FORMAT;
   }
-  
+
 }
--- gdmatrix-web/src/main/java/org/santfeliu/webapp/converters/DatePickerConverter.java
+++ gdmatrix-web/src/main/java/org/santfeliu/webapp/converters/DatePickerConverter.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.webapp.converters;
@@ -39,51 +39,57 @@ import javax.faces.convert.FacesConverter;
  * @author blanquepa
  */
 @FacesConverter(value = "datePickerConverter")
-public class DatePickerConverter extends DateConverter 
-{  
+public class DatePickerConverter extends DateConverter
+{
   public DatePickerConverter()
   {
   }
-  
+
   @Override
-  public String getAsString(FacesContext fc, UIComponent uic, 
-    String internalDate)    
+  public String getAsString(FacesContext fc, UIComponent uic,
+    String internalDate)
   {
     String userFormat = getUserFormat(uic);
-    
+
     return getAsString(fc, internalDate, userFormat);
   }
 
   @Override
   public String getAsObject(FacesContext fc, UIComponent uic, String userDate)
-  { 
+  {
     String userFormat = getUserFormat(uic);
-    String internalFormat = getInternalFormat(uic);  
-    
-    return getAsObject(fc, userDate, userFormat, internalFormat);
-  }  
-    
+    String internalFormat = getInternalFormat(uic);
+
+    String internalDate = getAsObject(fc, userDate, userFormat, internalFormat);
+    if (internalDate == null && !USER_DATE_FORMAT.equals(userFormat))
+    {
+      // try USER_DATE_FORMAT
+      internalDate = getAsObject(fc, userDate, USER_DATE_FORMAT, internalFormat);
+    }
+    return internalDate;
+  }
+
   private String getInternalFormat(UIComponent uic)
   {
     String internalFormat = INTERNAL_DATE_FORMAT;
     if (uic != null)
-    {  
-      Boolean showTime = (Boolean)uic.getAttributes().get("showTime"); 
-      Boolean timeOnly = (Boolean)uic.getAttributes().get("timeOnly");      
+    {
+      Boolean showTime = (Boolean)uic.getAttributes().get("showTime");
+      Boolean timeOnly = (Boolean)uic.getAttributes().get("timeOnly");
       if (showTime)
         internalFormat = INTERNAL_DATETIME_FORMAT;
       else if (timeOnly)
         internalFormat = INTERNAL_TIME_FORMAT;
-    }      
-      
+    }
+
     return internalFormat;
   }
-  
+
 
   private String getUserFormat(UIComponent uic)
   {
     String userFormat = USER_DATE_FORMAT;
-    
+
     if (uic != null)
     {
       String uFormat = (String)uic.getAttributes().get("pattern");
@@ -92,7 +98,7 @@ public class DatePickerConverter extends DateConverter
       else
       {
         Boolean showTime = (Boolean)uic.getAttributes().get("showTime");
-        Boolean timeOnly = (Boolean)uic.getAttributes().get("timeOnly");  
+        Boolean timeOnly = (Boolean)uic.getAttributes().get("timeOnly");
         if (showTime != null && showTime)
           userFormat = USER_DATETIME_FORMAT;
         else if (timeOnly != null && timeOnly)
@@ -100,7 +106,7 @@ public class DatePickerConverter extends DateConverter
       }
     }
     return userFormat;
-  }  
-  
-  
+  }
+
+
 }
