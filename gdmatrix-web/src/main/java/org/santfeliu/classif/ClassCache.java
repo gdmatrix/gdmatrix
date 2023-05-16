46c615d3353b673a9b6eab75be8f7454661531ec
realor <realor@santfeliu.cat>
2023-05-16 14:13:21 +0200

    Increase LRU size of ClassCache to 3000 entries.

--- gdmatrix-web/src/main/java/org/santfeliu/classif/ClassCache.java
+++ gdmatrix-web/src/main/java/org/santfeliu/classif/ClassCache.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.classif;
@@ -59,7 +59,7 @@ public class ClassCache
     new HashMap<String, ClassCache>();
 
   private String dateTime;
-  private LRUMap classMap = new LRUMap(1000); // 1000 Classes
+  private LRUMap classMap = new LRUMap(3000); // 3000 Classes
   private HashMap<String, List> childrenMap = new HashMap<String, List>();
   private HashMap<String, String> parentsMap = new HashMap<String, String>();
 
@@ -80,7 +80,7 @@ public class ClassCache
     this.lastPurgeMillis = nowMillis;
     this.lastAccessMillis = nowMillis;
   }
-  
+
   public static synchronized ClassCache getInstance()
   {
     return getInstance(ClassificationConfigBean.getDefaultDateTime());
@@ -94,7 +94,7 @@ public class ClassCache
   public static synchronized ClassCache getInstance(String dateTime)
   {
     purgeAbandonedInstances();
-    if (dateTime == null) dateTime = 
+    if (dateTime == null) dateTime =
       ClassificationConfigBean.getDefaultDateTime();
     ClassCache cache = classCaches.get(dateTime);
     if (cache == null)
@@ -155,6 +155,8 @@ public class ClassCache
     List<String> children = childrenMap.get(superClassId);
     if (children == null)
     {
+      System.out.println("\n\n>>>children NOT found for: " + superClassId);
+
       children = new ArrayList<String>();
       childrenMap.put(superClassId, children);
 
@@ -170,6 +172,10 @@ public class ClassCache
         children.add(subClass.getClassId());
         parentsMap.put(subClass.getClassId(), superClassId);
       }
+    }
+    else
+    {
+      System.out.println("\n\n>>>children found in cache for: " + superClassId);
     }
     return children;
   }
