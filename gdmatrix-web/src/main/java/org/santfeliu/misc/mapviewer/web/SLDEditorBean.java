c11f3ccbc2d79d4988b6199f154e3fe5a4c77bff
realor <realor@santfeliu.cat>
2023-08-10 19:46:19 +0200

    Hide point symbolizer icon for chart resources in SLDEditor.

--- gdmatrix-web/src/main/java/org/santfeliu/misc/mapviewer/web/SLDEditorBean.java
+++ gdmatrix-web/src/main/java/org/santfeliu/misc/mapviewer/web/SLDEditorBean.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.misc.mapviewer.web;
@@ -224,7 +224,7 @@ public class SLDEditorBean extends WebBean implements Savable
       (SLDPointSymbolizer)getValue("#{symbolizer}");
     SLDGraphic graphic = symbolizer.getGraphic();
     SLDMark mark = graphic.getMark();
-    
+
     StringBuilder buffer = new StringBuilder();
     if (!StringUtils.isBlank(mark.getWellKnownName()))
     {
@@ -240,7 +240,9 @@ public class SLDEditorBean extends WebBean implements Savable
     }
     SLDExternalGraphic externalGraphic = graphic.getExternalGraphic();
     String resource = externalGraphic.getOnlineResource();
-    if (!StringUtils.isBlank(resource))
+    if (!StringUtils.isBlank(resource) &&
+        !resource.contains("//chart?") &&
+        !resource.contains("${"))
     {
       if (!StringUtils.isBlank(graphic.getSizeAsXml()))
       {
@@ -355,7 +357,7 @@ public class SLDEditorBean extends WebBean implements Savable
     String strokeColor = stroke.getStrokeColor();
     String strokeWidth = stroke.getStrokeWidth();
     StringBuilder buffer = new StringBuilder();
-    if (!StringUtils.isBlank(fillColor) || 
+    if (!StringUtils.isBlank(fillColor) ||
         !StringUtils.isBlank(strokeColor) ||
         !StringUtils.isBlank(strokeWidth))
     {
@@ -500,7 +502,7 @@ public class SLDEditorBean extends WebBean implements Savable
     textSymbolizer.setCustomData("exp");
   }
 
-  public String editSLD(String sldName, 
+  public String editSLD(String sldName,
     List<String> layers, List<String> styles)
   {
     try
@@ -595,7 +597,7 @@ public class SLDEditorBean extends WebBean implements Savable
   }
 
   // private methods
-  
+
   private void addScriptFile(String path, StringBuilder buffer)
   {
     String contextPath = getContextPath();
