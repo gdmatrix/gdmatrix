2d534ecccbc8dc155706dc46cd8e46a46dcc82c7
realor <realor@santfeliu.cat>
2023-12-13 20:18:29 +0100

    Advance in geo module. -Base Panel control implementation. -Basic version of LegendPanel.

--- gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/maplibre-stub.js
+++ gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/maplibre-stub.js
@@ -14,6 +14,7 @@ if (window.mapLibreControlsLoaded === undefined)
       const div = document.createElement("div");
       div.className = "maplibregl-ctrl maplibregl-ctrl-group";
       div.innerHTML = `<button><span class="fa fa-home"/></button>`;
+      div.title = "Initial view";
       div.addEventListener("contextmenu", (e) => e.preventDefault());
       div.addEventListener("click", (e) =>
       {
@@ -29,31 +30,47 @@ if (window.mapLibreControlsLoaded === undefined)
   {
     constructor()
     {
-      this.pendingCount = 0;
+      this.tileKeys = new Set();
+      this.errorCount = 0;
     }
 
-    loadingStarted(event)
+    onLoadingStarted(event)
     {
-      if (event?.tile?.tileID?.canonical?.key)
+      let key = event?.tile?.tileID?.canonical?.key;
+      if (key)
       {
-        this.pendingCount++;
+        this.tileKeys.add(key);
         this.updateProgress();
       }
     }
 
-    loadingCompleted(event)
+    onLoadingCompleted(event)
     {
-      if (event?.tile?.tileID?.canonical?.key)
+      let key = event?.tile?.tileID?.canonical?.key;
+      if (key)
       {
-        this.pendingCount--;
+        this.tileKeys.delete(key);
+        this.updateProgress();
+      }
+    }
+
+    onError(event)
+    {
+      let key = event?.tile?.tileID?.canonical?.key;
+      if (key)
+      {
+        this.errorCount++;
+        console.error(event.error.message + ": " + event.source.tiles);
+        this.tileKeys.delete(key);
         this.updateProgress();
       }
     }
 
     updateProgress()
     {
-      this.div.textContent = this.pendingCount;
-      if (this.pendingCount > 0)
+      let pendingCount = this.tileKeys.size;
+      this.div.textContent = pendingCount;
+      if (pendingCount > 0)
       {
         this.div.classList.add("flash");
       }
@@ -61,26 +78,335 @@ if (window.mapLibreControlsLoaded === undefined)
       {
         this.div.classList.remove("flash");
       }
+      if (this.errorCount > 0)
+      {
+        this.div.style.color = "red";
+      }
+      else
+      {
+        this.div.style.color = "black";
+      }
     }
 
     onAdd(map)
     {
-      map.on("dataloading", (e) => this.loadingStarted(e));
-      map.on("data", (e) => this.loadingCompleted(e));
-      map.on("dataabort", (e) => this.loadingCompleted(e));
+      map.on("dataloading", (e) => this.onLoadingStarted(e));
+      map.on("data", (e) => this.onLoadingCompleted(e));
+      map.on("dataabort", (e) => this.onLoadingCompleted(e));
+      map.on("error", (e) => this.onError(e));
 
       const div = document.createElement("div");
       this.div = div;
       div.className = "maplibregl-ctrl maplibregl-ctrl-group flex align-items-center justify-content-center";
       div.style.width = "29px";
       div.style.height = "29px";
+      div.style.userSelect = "none";
       div.style.fontFamily = "var(--font-family)";
+      div.title = "Pending tiles";
       return div;
     }
   }
 
-  window.HomeButton = HomeButton;
-  window.LoadingIndicator = LoadingIndicator;
+  class Panel
+  {
+    constructor(containerId, title, iconClassName, insertTop = false)
+    {
+      this.container = containerId ? 
+        document.getElementById(containerId) : map.getContainer();
+      this.title = title;
+      this.iconClassName = iconClassName || "pi pi-info-circle";
+      this.insertTop = insertTop;
+    }
+
+    show()
+    {
+      this.panelDiv.style.display = "";
+      this.container.classList.add("expanded");
+      this.panelDiv.scrollIntoView({ behavior : "smooth" });
+    }
+
+    hide()
+    {
+      this.panelDiv.style.display = "none";
+      let panels = this.container.getElementsByClassName("panel");
+      let visiblePanel = false;
+      for (let panel of panels)
+      {
+        if (panel.style.display !== "none")
+        {
+          visiblePanel = true;
+          break;
+        }
+      }
+      if (!visiblePanel)
+      {
+        this.container.classList.remove("expanded");
+      }
+    }
+
+    onAdd(map)
+    {
+      this.map = map;
+      this.createPanel();
+
+      const div = document.createElement("div");
+      this.div = div;
+      div.innerHTML = `<button><span class="${this.iconClassName}"/></button>`;
+      div.className = "maplibregl-ctrl maplibregl-ctrl-group flex align-items-center justify-content-center";
+      div.title = this.title;
+      div.style.width = "29px";
+      div.style.height = "29px";
+      div.style.fontFamily = "var(--font-family)";
+      div.addEventListener("contextmenu", (e) => e.preventDefault());
+      div.addEventListener("click", (e) =>
+      {
+        e.preventDefault();
+        this.show();
+      });
+      
+      return div;
+    }
+
+    createPanel()
+    {
+      const panelDiv = document.createElement("div");
+      this.panelDiv = panelDiv;
+      panelDiv.className = "panel";
+      panelDiv.style.display = "none";
+      let firstPanel = this.container.firstElementChild.firstElementChild;
+      if (firstPanel && this.insertTop)
+      {
+        this.container.firstElementChild.insertBefore(panelDiv, firstPanel);        
+      }
+      else
+      {
+        this.container.firstElementChild.appendChild(panelDiv);
+      }
+
+      const headerDiv = document.createElement("div");
+      this.headerDiv = headerDiv;
+      headerDiv.className = "header flex-grow-0 flex p-1";
+      panelDiv.appendChild(headerDiv);
+      this.createHeader(headerDiv);
+
+      const bodyDiv = document.createElement("div");
+      this.bodyDiv = bodyDiv;
+      bodyDiv.className = "body flex-grow-1 overflow-auto body";
+      panelDiv.appendChild(bodyDiv);
+      this.createBody(bodyDiv);
+    }
+
+    createHeader(headerDiv)
+    {
+      const titleDiv = document.createElement("div");
+      titleDiv.className = "flex-grow-1";
+      headerDiv.appendChild(titleDiv);
+      this.createTitle(titleDiv);
+
+      const closeButton = document.createElement("button");
+      closeButton.className = "panel_button flex-grow-0";
+      closeButton.innerHTML = `<span class="pi pi-times-circle"></span>`;
+      headerDiv.appendChild(closeButton);
+
+      closeButton.addEventListener("click", (event) => 
+      {
+        event.preventDefault();
+        this.hide();
+      });
+    }
+
+    createBody(bodyDiv)
+    {
+    }
+
+    createTitle(titleDiv)
+    {
+      titleDiv.innerHTML = `<span class="${this.iconClassName} mr-1"></span> <span>${this.title}</span>`;
+    }
+  }
+
+  class SearchPanel extends Panel
+  {
+    constructor(containerId, insertTop)
+    {
+      super(containerId, "Search", "pi pi-search", insertTop);
+    }
+  }
+  
+  class InfoPanel extends Panel
+  {
+    constructor(containerId, insertTop)
+    {
+      super(containerId, "Information", "pi pi-info-circle", insertTop);
+    }
+
+    createBody(bodyDiv)
+    {
+      for (let i = 0; i < 20; i++)
+      {
+        let div = document.createElement("div");
+        div.innerHTML = `<span>bla bla bla bla bla</span>`;
+        bodyDiv.appendChild(div);
+      }
+    }  
+  }
+
+  class LegendPanel extends Panel
+  {
+    constructor(containerId, insertTop)
+    {
+      super(containerId, "Legend", "fa fa-layer-group", insertTop);
+    }
+    
+    createBody(bodyDiv)
+    {
+      setTimeout(() => this.populateTree(), 0);
+    }
+    
+    populateTree()
+    {
+      const style = this.map.getStyle();
+      const legend = style.metadata.legend;
+      if (legend)
+      {
+        this.titleDiv = legend.label;
+        if (legend.children && legend.children.length > 0)
+        {
+          const ul = document.createElement("ul");
+          ul.className = "legend";
+          this.bodyDiv.appendChild(ul);
+
+          for (let childNode of legend.children)
+          {
+            this.populateNode(childNode, ul);
+            this.updateLinks(childNode);
+          }
+        }       
+      }
+    }
+    
+    populateNode(node, ul)
+    {
+      const li = document.createElement("li");
+      ul.appendChild(li);
+      const liDiv = document.createElement("div");
+      liDiv.innerHTML = `<a href="#"><span></span> <span>${node.label}</span></a>`;
+      const link = liDiv.firstElementChild;
+      node.link = link;
+      link.addEventListener("click", (event) => 
+      {
+        event.preventDefault();
+        this.toggleNodeVisibility(node);
+        this.updateLinks(node);
+      });
+      li.appendChild(liDiv);
+      
+      if (node.children && node.children.length > 0)
+      {
+        const subul = document.createElement("ul");
+        li.appendChild(subul);
+        for (let childNode of node.children)
+        {
+          this.populateNode(childNode, subul);  
+        }
+      }      
+    }
+    
+    updateLinks(node)
+    {
+      const link = node.link;
+      const layerId = node.layerId;
+      
+      if (layerId)
+      {
+        const iconSpan = link.firstElementChild;
+        const textSpan = iconSpan.nextElementSibling;
+
+        const layer = this.map.getLayer(layerId);
+        if (layer?.metadata?.visible === false)
+        {
+          textSpan.classList.add("hidden_layer");
+          iconSpan.className = "pi pi-eye-slash";
+        }
+        else
+        {
+          textSpan.classList.remove("hidden_layer");
+          iconSpan.className = "pi pi-eye";        
+        }
+      }
+      else // group
+      {
+        for (let childNode of node.children)
+        {
+          this.updateLinks(childNode);
+        }        
+      }
+    }
+    
+    toggleNodeVisibility(node, sourceSet = null)
+    {
+      const map = this.map;
+      const layerId = node.layerId || null;
+      if (sourceSet === null) sourceSet = new Set();
+      
+      if (layerId)
+      {
+        const layer = map.getLayer(layerId);
+        const sourceId = layer.source;
+        console.info("tooggle layer: " + layerId + " source:" + sourceId);
+
+        if (layer.metadata?.visible === false)
+        {
+          layer.metadata.visible = true;
+        }
+        else
+        {
+          layer.metadata.visible = false;        
+        }
+
+        if (layer?.metadata?.layers)
+        {
+          sourceSet.add(sourceId);
+        }
+        else
+        {
+          layer.layout.visibility = layer.metadata.visible ? "visible" : "none";
+        }
+      }
+      else // group node
+      {
+        for (let childNode of node.children)
+        {
+          this.toggleNodeVisibility(childNode, sourceSet);
+        }
+      }
+      for (let sourceId of sourceSet)
+      {
+        let source = map.getSource(sourceId);
+        let serviceParameters = map.getStyle().metadata.serviceParameters[sourceId];
+        let masterLayer = map.getLayer(serviceParameters.masterLayer);
+        if (masterLayer.layout === undefined) masterLayer.layout = {};
+
+        const sourceUrl = getSourceUrl(sourceId, map.getStyle());
+        if (sourceUrl === null)
+        {
+          masterLayer.layout.visibility = "none";          
+        }
+        else
+        {
+          source.setTiles([sourceUrl]);
+          masterLayer.layout.visibility = "visible";
+        }        
+      }
+    }
+  }
+
+  window.maplibreglx = {};
+  maplibreglx.HomeButton = HomeButton;
+  maplibreglx.LoadingIndicator = LoadingIndicator;
+  maplibreglx.SearchPanel = SearchPanel;
+  maplibreglx.InfoPanel = InfoPanel;
+  maplibreglx.LegendPanel = LegendPanel;
   window.mapLibreControlsLoaded = true;
 }
 
@@ -125,6 +451,11 @@ function getSourceUrl(sourceId, style)
   {
     if (layer.source === sourceId && layer.metadata)
     {
+      if (serviceParameters.masterLayer === undefined)
+      {
+        serviceParameters.masterLayer = layer.id;
+      }
+      
       if (layer.metadata.visible)
       {
         if (layer.metadata.layers?.length > 0)
@@ -205,7 +536,6 @@ function getSourceUrl(sourceId, style)
   {
     url = serviceUrl + "?" + urlParams;
   }
-  console.info(url);
   return url;
 }
 
@@ -275,12 +605,13 @@ function maplibreInit(clientId, style)
     maxZoom: style.metadata.maxZoom,
     maxPitch: style.metadata.maxPitch,
     hash: style.metadata.hash || false,
-    style: style
+    style: style,
+    antialias: true
   });
   window.map = map;
+  map.setPixelRatio(window.devicePixelRatio);
 
-  map.addControl(
-    new maplibregl.NavigationControl({
+  map.addControl(new maplibregl.NavigationControl({
       visualizePitch: true,
       showZoom: true,
       showCompass: true
@@ -299,7 +630,7 @@ function maplibreInit(clientId, style)
     unit: 'metric'
   }));
 
-  map.addControl(new HomeButton({
+  map.addControl(new maplibreglx.HomeButton({
     center: style.center,
     zoom: style.zoom,
     bearing: style.bearing,
@@ -309,7 +640,15 @@ function maplibreInit(clientId, style)
     easing(t) { return t; }
   }), "top-right");
 
-  map.addControl(new LoadingIndicator(), "top-right");
+  map.addControl(new maplibreglx.LoadingIndicator(), "top-right");
+
+  map.addControl(new maplibreglx.SearchPanel("maplibre_left_container"), "top-left");
+
+  map.addControl(new maplibreglx.InfoPanel("maplibre_left_container"), "top-left");
+
+  map.addControl(new maplibreglx.LegendPanel("maplibre_right_container", true), "bottom-right");
+
+  map.addControl(new maplibreglx.InfoPanel("maplibre_right_container", true), "bottom-right");
 
   if (style.terrain)
   {
