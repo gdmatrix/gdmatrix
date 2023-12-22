fd0ced541253bd479ff45bbb6cb0ef0dd318115a
realor <realor@santfeliu.cat>
2023-12-22 13:55:49 +0100

    New ExportArea tool in geo module.

--- gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/maplibre-stub.js
+++ gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/maplibre-stub.js
@@ -699,12 +699,12 @@ if (window.mapLibreControlsLoaded === undefined)
       const map = this.map;
       map.off("click", this._onMapClick);
       map.getCanvas().style.cursor = "grab";
-      this.headerDiv.innerHTML = "";      
+      this.headerDiv.innerHTML = "";
       this.infoDiv.innerHTML = "";
       this.clearHighlight();
       this.panel.hide();
     }
-    
+
     createPanel()
     {
       this.headerDiv = document.createElement("div");
@@ -719,7 +719,7 @@ if (window.mapLibreControlsLoaded === undefined)
       const map = this.map;
       const infoDiv = this.infoDiv;
       infoDiv.innerHTML = "";
-      
+
       const ul = document.createElement("ul");
       ul.className = "feature_info";
       infoDiv.appendChild(ul);
@@ -829,8 +829,10 @@ if (window.mapLibreControlsLoaded === undefined)
         "layout": {},
         "paint":
         {
+          "circle-opacity": 0,
           "circle-radius": 6,
-          "circle-color": "#0000ff"
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "blue"
         },
         "filter": ['==', '$type', 'Point']
       });
@@ -885,7 +887,7 @@ if (window.mapLibreControlsLoaded === undefined)
         const utmDiv = document.createElement("div");
         headerDiv.appendChild(utmDiv);
         utmDiv.innerHTML = `<div>UTM-x: ${utm.easting}</div>
-                            <div>UTM-y: ${utm.northing}</div>`;      
+                            <div>UTM-y: ${utm.northing}</div>`;
       }
 
       this.infoDiv.innerHTML = `<span class="pi pi-spin pi-spinner pt-4 pb-4" />`;
@@ -956,7 +958,7 @@ if (window.mapLibreControlsLoaded === undefined)
       return FeatureTypeInspector.getInfo(service.url, layerName).then(info =>
       {
         const map = this.map;
-        const dist = 10 / map.getZoom();
+        const dist = 20 / map.getZoom();
 
         if (info.geometryColumn)
         {
@@ -1164,8 +1166,7 @@ if (window.mapLibreControlsLoaded === undefined)
       bodyDiv.appendChild(this.resultDiv);
     }
   }
-  
-  
+
   class MeasureArea extends Tool
   {
     constructor(containerId, insertTop)
@@ -1282,18 +1283,18 @@ if (window.mapLibreControlsLoaded === undefined)
 
         let utm1 = toUtm(c1[1], c1[0], 7, 'ETRS89');
         let utm2 = toUtm(c2[1], c2[0], 7, 'ETRS89');
-        
+
         let x1 = utm1.easting;
         let y1 = utm1.northing;
 
         let x2 = utm2.easting;
-        let y2 = utm2.northing;        
-        
+        let y2 = utm2.northing;
+
         area += (0.5 * (y1 + y2) * (x2 - x1));
       }
-      
+
       area = Math.abs(area);
-      
+
       this.resultDiv.textContent = "Area: " + area.toFixed(3) + " m2";
       this.resultDiv.className = "p-4";
     }
@@ -1341,7 +1342,280 @@ if (window.mapLibreControlsLoaded === undefined)
       this.resultDiv = document.createElement("div");
       bodyDiv.appendChild(this.resultDiv);
     }
-  }  
+  }
+
+  class ExportArea extends Tool
+  {
+    constructor(containerId, insertTop)
+    {
+      super("pi pi-download", "Export area");
+      this.panel = new Panel(containerId, "Export area", "pi pi-info-circle", insertTop);
+      this.createPanel();
+      this._onMapClick = (event) => this.addPoint(event.lngLat);
+      this.startData = {
+        "type": "Feature",
+        "geometry": { "type": "Point", "coordinates": [] }
+      };
+      this.data = {
+        "type": "Feature",
+        "geometry": { "type": "Polygon", "coordinates": [[]] }
+      };
+      this.formats = [
+        ["GEOJSON", "application/json", "geojson"],
+        ["GML3", "gml3", "xml"],
+        ["EXCEL", "excel", "xlsx"],
+        ["CSV", "text/csv", "csv"],
+        ["SHAPE", "SHAPE-ZIP", "zip"],       
+        ["DXF", "DXF", "dxf"],
+        ["DXF-ZIP", "DXF-ZIP", "zip"],
+        ["KML", "application/vnd.google-earth.kml+xml", "kml"]
+      ];
+    }
+
+    activate()
+    {
+      const map = this.map;
+      map.on("click", this._onMapClick);
+      map.getCanvas().style.cursor = "crosshair";
+      this.panel.show();
+
+      map.addSource("measured_polygon_start", {
+        type: 'geojson',
+        data: this.startData
+      });
+
+      map.addLayer({
+        "id": "measured_polygon_start",
+        "type": 'circle',
+        "source": "measured_polygon_start",
+        "layout": {},
+        "paint":
+        {
+          "circle-color": "#000000",
+          "circle-radius": 4
+        }
+      });
+
+      map.addSource("measured_polygon", {
+        type: 'geojson',
+        data: this.data
+      });
+
+      map.addLayer({
+        "id": "measured_polygon",
+        "type": 'fill',
+        "source": "measured_polygon",
+        "layout": {},
+        "paint":
+        {
+          "fill-color": "#0000ff",
+          "fill-opacity": 0.2
+        }
+      });
+
+      map.addLayer({
+        "id": "measured_polygon_points",
+        "type": 'circle',
+        "source": "measured_polygon",
+        "layout": {},
+        "paint":
+        {
+          "circle-color": "#000000",
+          "circle-radius": 3
+        }
+      });
+    }
+
+    deactivate()
+    {
+      const map = this.map;
+      map.off("click", this._onMapClick);
+      map.getCanvas().style.cursor = "grab";
+      this.panel.hide();
+
+      map.removeLayer("measured_polygon_start");
+      map.removeSource("measured_polygon_start");
+
+      map.removeLayer("measured_polygon");
+      map.removeLayer("measured_polygon_points");
+      map.removeSource("measured_polygon");
+    }
+
+    addPoint(lngLat)
+    {
+      const map = this.map;
+
+      if (this.startData.geometry.coordinates.length === 0)
+      {
+        this.startData.geometry.coordinates = [lngLat.lng, lngLat.lat];
+        map.getSource("measured_polygon_start").setData(this.startData);
+      }
+
+      this.data.geometry.coordinates[0].push([lngLat.lng, lngLat.lat]);
+      map.getSource("measured_polygon").setData(this.data);
+
+      this.panel.show();
+      this.resultDiv.innerHTML = "";
+    }
+
+    createPanel()
+    {
+      const bodyDiv = this.panel.bodyDiv;
+      const buttonBar = document.createElement("div");
+      buttonBar.className = "button_bar";
+      bodyDiv.appendChild(buttonBar);
+
+      const clearButton = document.createElement("button");
+      clearButton.textContent = "Reset";
+      clearButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.startData.geometry.coordinates = [];
+        map.getSource("measured_polygon_start").setData(this.startData);
+
+        this.data.geometry.coordinates[0] = [];
+        map.getSource("measured_polygon").setData(this.data);
+        this.resultDiv.innerHTML = "";
+      });
+      buttonBar.appendChild(clearButton);
+
+      const undoButton = document.createElement("button");
+      undoButton.textContent = "Undo";
+      undoButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (this.data.geometry.coordinates[0].length > 0)
+        {
+          this.data.geometry.coordinates[0].pop();
+          map.getSource("measured_polygon").setData(this.data);
+
+          if (this.data.geometry.coordinates[0].length === 0)
+          {
+            this.startData.geometry.coordinates = [];
+            map.getSource("measured_polygon_start").setData(this.startData);
+          }
+        }
+        this.resultDiv.innerHTML = "";
+      });
+      buttonBar.appendChild(undoButton);
+
+      const exportButton = document.createElement("button");
+      exportButton.textContent = "Export";
+      exportButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.exportLayers();
+      });
+      buttonBar.appendChild(exportButton);
+
+      this.resultDiv = document.createElement("div");
+      bodyDiv.appendChild(this.resultDiv);
+    }
+
+    exportLayers()
+    {
+      const map = this.map;
+      const services = map.getStyle().metadata?.services;
+      const serviceParameters = map.getStyle().metadata?.serviceParameters;
+      if (services === undefined) return;
+      if (serviceParameters === undefined) return;
+
+      let promises = [];
+
+      for (let layer of map.getStyle().layers)
+      {
+        if (layer.metadata?.visible && layer.metadata?.locatable)
+        {
+          let sourceId = layer.source;
+          let params = serviceParameters[sourceId];
+          if (params)
+          {
+            let serviceId = params.service;
+            if (serviceId)
+            {
+              let service = services[serviceId];
+              let layerNameArray = [];
+              if (layer.metadata?.layers)
+              {
+                layerNameArray.push(...layer.metadata.layers.split(","));
+              }
+              if (params.layers)
+              {
+                layerNameArray.push(...params.layers.split(","));
+              }
+              for (let layerName of layerNameArray)
+              {
+                promises.push(FeatureTypeInspector.getInfo(service.url, layerName)
+                  .then(info =>
+                  {
+                    return {
+                      serviceUrl: service.url, 
+                      layerId : layer.id,
+                      layerName: layerName, 
+                      layerLabel: layer.metadata?.label,
+                      geometryColumn: info.geometryColumn 
+                    };
+                  }));
+              }
+            }
+          }
+        }
+      }
+      Promise.all(promises).then(layerInfo => this.showLinks(layerInfo));
+    }
+    
+    showLinks(layerInfo)
+    {
+      let cqlPolygon = "SRID=4326;POLYGON((";
+      let ring = this.data.geometry.coordinates[0];
+      if (ring.length < 3) return;
+
+      for (let i = 0; i < ring.length; i++)
+      {
+        let point = ring[i];
+        if (i > 0) cqlPolygon += ", ";
+        cqlPolygon += point[0] + " " + point[1];
+      }
+      cqlPolygon += ", " + ring[0][0] + " "  + ring[0][1]; // add first point
+      cqlPolygon += "))";
+      
+      const formats = this.formats;
+      
+      const resultDiv = this.resultDiv;
+      resultDiv.innerHTML = "";
+      
+      for (let info of layerInfo)
+      {
+        if (info.geometryColumn === null) continue;
+
+        let serviceUrl = info.serviceUrl;
+        let layerId = info.layerId;
+        let layerName = info.layerName;
+        let layerLabel = info.layerLabel || layerId || layerName;
+        let cqlFilter = "INTERSECTS(" + info.geometryColumn +
+          ", " + cqlPolygon + ")";
+  
+
+        let layerDiv = document.createElement("div");
+        layerDiv.className = "pt-2";
+        layerDiv.innerHTML = `<div class="mb-2"><b>${layerLabel}</b> (${layerName}):</div>`;
+        resultDiv.appendChild(layerDiv);
+
+        for (let format of formats)
+        {
+          let url = "/proxy?url=" + serviceUrl +
+            "&service=WFS&version=2.0.0&request=GetFeature&typeName=" +
+              layerName + "&outputFormat=" + format[1] +
+              "&srsName=EPSG:25831" +
+              "&cql_filter=" + encodeURIComponent(cqlFilter);
+          let anchor = document.createElement("a");
+          anchor.target= "_blank";
+          anchor.href = url;
+          anchor.className = "export_link";
+          anchor.download = layerId + "." + format[2];
+          anchor.textContent = format[0];
+          layerDiv.append(anchor);
+        }
+      }
+    }
+  }
 
   window.maplibreglx = {};
   maplibreglx.HomeButton = HomeButton;
@@ -1352,6 +1626,7 @@ if (window.mapLibreControlsLoaded === undefined)
   maplibreglx.GetFeatureInfo = GetFeatureInfo;
   maplibreglx.MeasureLength = MeasureLength;
   maplibreglx.MeasureArea = MeasureArea;
+  maplibreglx.ExportArea = ExportArea;
 
   window.mapLibreControlsLoaded = true;
 }
@@ -1627,6 +1902,8 @@ function maplibreInit(clientId, style)
   map.addControl(new maplibreglx.MeasureLength("maplibre_right_container", true), "top-left");
 
   map.addControl(new maplibreglx.MeasureArea("maplibre_right_container", true), "top-left");
+
+  map.addControl(new maplibreglx.ExportArea("maplibre_right_container", true), "top-left");
 
   map.addControl(new maplibreglx.Legend("maplibre_right_container", true), "bottom-right");
 
