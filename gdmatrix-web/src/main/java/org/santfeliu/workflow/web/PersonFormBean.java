7d79f9cede0ac9bbce0f79ace188b3b857faf199
realor <realor@santfeliu.cat>
2024-01-12 10:22:47 +0100

    Call to kernel as admin user in PersonFormBean.

--- gdmatrix-web/src/main/java/org/santfeliu/workflow/web/PersonFormBean.java
+++ gdmatrix-web/src/main/java/org/santfeliu/workflow/web/PersonFormBean.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.workflow.web;
@@ -52,6 +52,7 @@ import org.matrix.kernel.Sex;
 import org.matrix.util.WSDirectory;
 import org.matrix.util.WSEndpoint;
 import org.santfeliu.faces.FacesUtils;
+import org.santfeliu.util.MatrixConfig;
 import org.santfeliu.util.Properties;
 import org.santfeliu.web.UserSessionBean;
 import org.santfeliu.workflow.form.Form;
@@ -328,7 +329,7 @@ public class PersonFormBean extends FormBean
       result.add(row);
     }
 
-    if (isCreationAllowed() && rows != null 
+    if (isCreationAllowed() && rows != null
       && (isIdentityRequired() && !(StringUtils.isBlank(NIF) && StringUtils.isBlank(passport))
           || !isIdentityRequired()) && creationPersonTypeId != null)
     {
@@ -819,9 +820,11 @@ public class PersonFormBean extends FormBean
   {
     try
     {
+      String userId = MatrixConfig.getProperty("adminCredentials.userId");
+      String password = MatrixConfig.getProperty("adminCredentials.password");
       WSDirectory dir = WSDirectory.getInstance();
       WSEndpoint endpoint = dir.getEndpoint(KernelManagerService.class);
-      return endpoint.getPort(KernelManagerPort.class);
+      return endpoint.getPort(KernelManagerPort.class, userId, password);
     }
     catch (Exception ex)
     {
