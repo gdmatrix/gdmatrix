3b215291388307af3b9346afd9dda0bb5e208bb0
lopezrj-sf <lopezrj@ext.santfeliu.cat>
2024-03-22 09:26:00 +0100

    Allow exclusion of special characters in the Unicode.encode method.

--- gdmatrix-common/src/main/java/org/santfeliu/util/enc/Unicode.java
+++ gdmatrix-common/src/main/java/org/santfeliu/util/enc/Unicode.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.util.enc;
@@ -40,8 +40,13 @@ public class Unicode
   {
     return encode(text, false);
   }
-  
+
   public static String encode(String text, boolean extendedASCII)
+  {
+    return (extendedASCII ? encode(text, 0, 255) : encode(text, 32, 127));
+  }
+
+  public static String encode(String text, int ignoreCodeMin, int ignoreCodeMax)
   {
     if (text == null) return null;
 
@@ -52,7 +57,7 @@ public class Unicode
       {
         buffer.append("\\\\");
       }
-      else if (ch >= 32 && ch < (extendedASCII ? 256 : 128))
+      else if (ch >= ignoreCodeMin && ch <= ignoreCodeMax)
       {
         buffer.append(ch);
       }
