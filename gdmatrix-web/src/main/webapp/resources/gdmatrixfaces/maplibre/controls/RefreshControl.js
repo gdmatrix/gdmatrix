effe7f9be592c0e9cc6c22c400bcf7e069d7b8a3
realor <realor@santfeliu.cat>
2024-05-13 17:44:54 +0200

    Improve RefreshControl to update sources at regular intervals.

--- gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/controls/RefreshControl.js
+++ gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/controls/RefreshControl.js
@@ -6,8 +6,16 @@ const bundle = Bundle.getBundle("main");
 
 class RefreshControl
 {
-  constructor()
+  constructor(options)
   {
+    this.defaultRefreshInterval = options?.defaultRefreshInterval || 0; // seconds
+    this.refreshIntervalMap = options?.refreshIntervalMap || {}; // { "sourceId": <seconds | 0>, ...}
+    this.lastRefreshTimeMap = {}; // { "sourceId": millis, ...}
+
+    // value for refreshInterval = x:
+    // x < 0: never refresh
+    // x == 0: only refresh manually (pressing button).
+    // x > 0: refresh automatically every x seconds
   }
 
   onAdd(map)
@@ -21,35 +29,101 @@ class RefreshControl
     div.addEventListener("click", (e) =>
     {
       e.preventDefault();
-      this.refreshSources();
+      this.refreshSources(false);
     });
 
+    this.initLastRefreshTimes();
+
+    const update = () => {      
+      this.refreshSources(true);
+      if (document.body.contains(map.getCanvas()))
+      {
+        setTimeout(update, 1000);
+      }
+    };
+
+    if (this.isTimerRequired())
+    {
+      update();
+    }
     return div;
   }
-  
-  refreshSources()
+
+  refreshSources(auto = false)
   {
     const map = this.map;
     const sources = map.getStyle().sources;
     const seed = "_seed=" + Math.random();
+
     for (let sourceId in sources)
     {
-      let source = sources[sourceId];
-      if (source.type === "geojson" && typeof source.data === "string")
+      if (this.isRefreshRequired(sourceId, auto))
       {
-        let url = source.data;
-        if (url.indexOf("?") === -1)
+        let source = sources[sourceId];
+        if (source.type === "geojson" && typeof source.data === "string")
         {
-          url += "?" + seed;
+          let url = source.data;
+          if (url.indexOf("?") === -1)
+          {
+            url += "?" + seed;
+          }
+          else
+          {
+            url += "&" + seed;
+          }
+          console.info("Refresh " + sourceId);
+          map.getSource(sourceId).setData(url);
+          this.lastRefreshTimeMap[sourceId] = Date.now();
         }
-        else
-        {
-          url += "&" + seed;
-        }
-        console.info(url);
-        map.getSource(sourceId).setData(url);
       }
     }
+  }
+  
+  isRefreshRequired(sourceId, auto)
+  {
+    const seconds = this.getRefreshInterval(sourceId);
+
+    if (auto)
+    {
+      if (seconds <= 0) return false;
+
+      const lastRefreshTime = this.lastRefreshTimeMap[sourceId];
+      const millis = Date.now();
+      return (millis - lastRefreshTime) > seconds * 1000;
+    }
+    else // manual
+    {
+      return seconds >= 0;
+    }
+  }
+
+  initLastRefreshTimes()
+  {
+    const sources = map.getStyle().sources;
+
+    const millis = Date.now();
+    for (let sourceId in sources)
+    {
+      this.lastRefreshTimeMap[sourceId] = millis;
+    }
+  }
+
+  isTimerRequired()
+  {
+    const sources = map.getStyle().sources;
+
+    for (let sourceId in sources)
+    {
+      if (this.getRefreshInterval(sourceId) > 0) return true;
+    }    
+    return false;
+  }
+  
+  getRefreshInterval(sourceId)
+  {
+    let seconds = this.refreshIntervalMap[sourceId];
+    if (seconds === undefined) seconds = this.defaultRefreshInterval;
+    return seconds;    
   }
 }
 
