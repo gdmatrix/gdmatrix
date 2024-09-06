68517aa262048eb0d528f77b9a735c1417602f44
realor <realor@santfeliu.cat>
2024-09-06 15:42:24 +0200

    Support Rhino ConsString instances in MapScriptable.

--- gdmatrix-common/src/main/java/org/santfeliu/util/script/MapScriptable.java
+++ gdmatrix-common/src/main/java/org/santfeliu/util/script/MapScriptable.java
@@ -1,37 +1,38 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.util.script;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.mozilla.javascript.ConsString;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.NativeJavaObject;
@@ -81,6 +82,10 @@ public class MapScriptable extends ScriptableObject
     {
       data.remove(name);
     }
+    else if (value instanceof ConsString)
+    {
+      data.put(name, String.valueOf(value));
+    }
     else if (value instanceof NativeJavaObject)
     {
       NativeJavaObject nativeJavaObject = (NativeJavaObject)value;
@@ -107,7 +112,7 @@ public class MapScriptable extends ScriptableObject
         entry.put("userid", "realor");
         entry.put("date", "20110801");
         entry.put("time", "121100");
-        
+
         DefaultScriptable scriptable = new DefaultScriptable(cx);
         scriptable.put("entry", scriptable, new MapScriptable(scriptable, entry));
 
