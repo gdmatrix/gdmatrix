3b7b72585b0e5f95fb53a8d958ec9de872bf56da
realor <realor@santfeliu.cat>
2024-10-18 14:38:14 +0200

    Avoid Exception when label is null in HtmlRendererUtils.

--- gdmatrix-web/src/main/java/org/santfeliu/faces/render/myfaces/menu/HtmlRendererUtils.java
+++ gdmatrix-web/src/main/java/org/santfeliu/faces/render/myfaces/menu/HtmlRendererUtils.java
@@ -1,31 +1,31 @@
 /*
  * GDMatrix
- *  
+ *
  * Copyright (C) 2020, Ajuntament de Sant Feliu de Llobregat
- *  
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- *  
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *    
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *    
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *    
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * http://www.gnu.org/licenses/ 
- * and 
+ * http://www.gnu.org/licenses/
+ * and
  * https://www.gnu.org/licenses/lgpl.txt
  */
 package org.santfeliu.faces.render.myfaces.menu;
@@ -493,13 +493,15 @@ public final class HtmlRendererUtils {
                 }
 
                 if (StringUtils.isBlank(selectItem.getLabel()))
-                  writer.writeAttribute("label", " ", null);  
-                
-                if (escape)                 
-                  writer.writeText(selectItem.getLabel(), null); 
-                else 
-                  writer.write(selectItem.getLabel());                 
+                  writer.writeAttribute("label", " ", null);
 
+                String label = selectItem.getLabel();
+                if (label == null) label = " ";
+
+                if (escape)
+                  writer.writeText(label, null);
+                else
+                  writer.write(label);
 
                 writer.endElement(HTML.OPTION_ELEM);
             }
