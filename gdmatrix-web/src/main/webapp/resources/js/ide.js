97a206bade1ccf9675e58b24f2140e8248d96143
realor <realor@santfeliu.cat>
2024-11-15 18:34:10 +0100

    Improvements in web IDE pages. -Refactor classes. -New JS Runner tab to execute scripts.

--- gdmatrix-web/src/main/webapp/resources/js/ide.js
+++ gdmatrix-web/src/main/webapp/resources/js/ide.js
@@ -32,3 +32,51 @@ function endPanel()
     closePanel();
   }
 }
+
+var readingConsole = false;
+
+async function readConsole(consoleId, clear = false)
+{
+  console.info("read " + consoleId);
+  readingConsole = true;
+
+  const consoleElem = document.getElementById("js_console");
+  if (!consoleElem) return;
+
+  if (clear)
+  {
+    consoleElem.innerHTML = "";
+  }
+
+  const END_TAG = 0;
+  const response = await fetch("/stream/" + consoleId);
+  const queue = await response.json();  
+  
+  for (var item of queue)
+  {
+    if (item === END_TAG)
+    { 
+      readingConsole = false;
+      if (updateToolbar) updateToolbar();
+    }
+    else
+    {
+      const msgElem = document.createElement("div");
+      msgElem.className = item.level;
+      for (let value of item.values)
+      {
+        const itemElem = document.createElement("div");
+        itemElem.className = typeof value;
+        itemElem.textContent = value;
+        msgElem.appendChild(itemElem);
+      }
+      consoleElem.appendChild(msgElem);
+      consoleElem.scrollTop = consoleElem.scrollHeight;
+    }
+  }
+
+  if (readingConsole)
+  {
+    setTimeout(() => readConsole(consoleId), 0);
+  }
+}
