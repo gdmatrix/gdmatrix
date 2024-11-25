c4a625e23861a93af1c08eb4ba8556e0a84b94f2
realor <realor@santfeliu.cat>
2024-11-25 09:39:37 +0100

    Add parameters to IDE to use HttpURLConnection under JDK 17.

--- gdmatrix-ide/src/main/deploy/run.bat
+++ gdmatrix-ide/src/main/deploy/run.bat
@@ -1,3 +1,3 @@
 cd %~dp0
-start javaw -cp "../lib/*" org.santfeliu.matrix.ide.MatrixIDE
+start javaw -cp "../lib/*" --add-opens java.base/sun.net.www.protocol.http=ALL-UNNAMED --add-opens java.base/sun.net.www.protocol.https=ALL-UNNAMED org.santfeliu.matrix.ide.MatrixIDE
 
