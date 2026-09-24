c8a81bf5e62e3b7b931bb78177be9ac66175802c
realor <realor@santfeliu.cat>
2026-09-24 09:54:43 +0200

    Support floating panel in CustomPanelControl.

--- gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/controls/CustomPanelControl.js
+++ gdmatrix-web/src/main/webapp/resources/gdmatrixfaces/maplibre/controls/CustomPanelControl.js
@@ -20,11 +20,25 @@ class CustomPanelControl
 
   createPanel(map)
   {
-    this.panel = new Panel(map, this.options);
-
-    if (typeof this.options.onShowPanel === "function")
+    const floatingPanel = this.options.floatingPanel;
+    if (floatingPanel)
     {
-      this.panel.onShow = () => this.options.onShowPanel(this);
+      this.floatingPanel = document.createElement("div");
+      this.div.appendChild(this.floatingPanel);
+      this.div.style.display = "flex";
+      this.div.style.flexDirection = "column";
+      this.floatingPanel.style.display = "none";
+      this.floatingPanel.style.padding = "4px";
+      this.floatingPanel.style.padding = "4px";
+      this.floatingPanel.style.overflow = "auto";
+    }
+    else
+    {
+      this.panel = new Panel(map, this.options);
+      if (typeof this.options.onShowPanel === "function")
+      {
+        this.panel.onShow = () => this.options.onShowPanel(this);
+      }
     }
   }
   
@@ -34,17 +48,40 @@ class CustomPanelControl
 
     const div = document.createElement("div");
     this.div = div;
-    div.innerHTML = `<button><span class="${this.options.iconClass}"/></button>`;
+    div.innerHTML = `<button style="flex-shrink:0"><span class="${this.options.iconClass}"/></button>`;
     div.className = "maplibregl-ctrl maplibregl-ctrl-group";
     div.title = this.options.title;
     div.style.width = "29px";
     div.style.height = "29px";
+    div.style.transition = "width 0.3s ease, height 0.3s ease";
     div.style.fontFamily = "var(--font-family)";
+    div.style.overflow = "hidden";
     div.addEventListener("contextmenu", (e) => e.preventDefault());
-    div.addEventListener("click", (e) =>
+    const button = div.querySelector("button");
+    button.addEventListener("click", (e) =>
     {
       e.preventDefault();
-      this.panel.show();
+      const floatingPanel = this.floatingPanel;
+      if (floatingPanel)
+      {
+        if (floatingPanel.style.display === "none")
+        {
+          div.style.width = this.options.floatingPanelWidth || "30vw";
+          div.style.height = this.options.floatingPanelHeight || "20vh";
+          floatingPanel.style.display = "block";
+          this.options.onShowPanel?.(this);
+        }
+        else
+        {
+          div.style.width = "29px";
+          div.style.height = "29px";
+          floatingPanel.style.display = "none";          
+        }
+      }
+      else
+      {
+        this.panel.show();
+      }
     });
 
     this.createPanel(map);
